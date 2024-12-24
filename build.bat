585454abfe32e5a23ad25c0ed1393e4ca02454a5
Yeshal Khan <yeshalkhan13@gmail.com>
2024-12-24 22:12:30 +0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 echo "Installing dependencies..."
 
-:: Use Python to call pip directly
 "C:\Users\computer point\AppData\Local\Programs\Python\Python37\python.exe" -m pip install -r requirements.txt
 
 echo "Build complete!"

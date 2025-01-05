dd9efef8c2cf12e04be4aa96873667f8437f3fb6
Yeshal Khan <yeshalkhan13@gmail.com>
2025-01-05 20:38:46 +0500

    Changed db connection string

--- tests/test_cases_ems.py
+++ tests/test_cases_ems.py
@@ -2,7 +2,6 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from dotenv import load_dotenv
-
 from app import app, format_response, login_required, admin_required
 import pytest
 from flask import session
@@ -10,6 +9,8 @@ from datetime import datetime
 from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
     
+load_dotenv()
+
 # Create a fixture to initialize the Flask app and MongoDB
 @pytest.fixture
 def client():
